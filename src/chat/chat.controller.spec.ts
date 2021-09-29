c5b89479354b6a4c4dbe7230b1115e33ba4cc2d1
kiken-eng <kanyecope@gmail.com>
2021-09-29 12:04:42 +0300

    Written tests for the sendMail chat feature

--- src/chat/chat.controller.spec.ts
+++ src/chat/chat.controller.spec.ts
@@ -1,20 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
+import mailChatDto from './dto/sendMail.dto';
 
 describe('ChatController', () => {
+  let service: ChatService;
+
   let controller: ChatController;
 
+  const mockChatService = {
+    mailAll: jest.fn().mockImplementation((createEmailDto: mailChatDto) => {
+      return { createEmailDto };
+    }),
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ChatController],
       providers: [ChatService],
-    }).compile();
+    })
+      .overrideProvider(ChatService)
+      .useValue(mockChatService)
+      .compile();
 
     controller = module.get<ChatController>(ChatController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('Email sending', async () => {
+    const data: mailChatDto = {
+      subject: 'Written test for Email Sending',
+      body:
+        'Test for the sending of an email, If you see this then the test has passed',
+      recipientId: [4, 5],
+    };
+    expect(controller.create(data));
+
+    expect(mockChatService.mailAll).toHaveBeenCalledWith(data);
   });
 });
--- src/chat/chat.service.spec.ts
+++ src/chat/chat.service.spec.ts
@@ -1,15 +1,33 @@
+import { ChatModule } from './chat.module';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ChatService } from './chat.service';
+import mailChatDto from './dto/sendMail.dto';
+import { provide } from 'vue';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import Email from 'src/crm/entities/email.entity';
 
 describe('ChatService', () => {
   let service: ChatService;
+  const mockEmailRepository = {
+    mailAll: jest.fn().mockImplementation((data) => data),
+    sendEmailToMembers: jest
+      .fn()
+      .mockImplementation((dto) =>
+        Promise.resolve({ recipientId: Date.now(), ...dto }),
+      ),
+  };
 
+  //include the getRepositoryToken for mocking repositories  and the "useValue to mock the repo"
+  //Remove the imports:[Module name]
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ChatService],
+      providers: [
+        ChatService,
+        { provide: getRepositoryToken(Email), useValue: mockEmailRepository },
+      ],
     }).compile();
 
-    service = module.get<ChatService>(ChatService);
+    service = await module.get<ChatService>(ChatService);
   });
 
   it('should be defined', () => {
